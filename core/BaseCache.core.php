bb95eadb98f9af7f8087f6e8bcadd88807f5acf6
Bradley Hamilton <bradleygoskimmin@gmail.com>
2014-02-28 15:08:09 -0500

    Added constructor for connecting to memcache server

--- core/BaseCache.core.php
+++ core/BaseCache.core.php
@@ -1,6 +1,10 @@
 <?php
 
 class BaseCache extends Memcache {
+
+    public function __construct(){
+        $this->connect($_SERVER['MEMCACHE_HOST'],$_SERVER['MEMCACHE_PORT']);
+    }//construct
     
 }//BaseCache
 
