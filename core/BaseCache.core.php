c6c73183ae3745474547f62ead236813f3e54225
Bradley Hamilton <bradleygoskimmin@gmail.com>
2014-02-28 15:33:19 -0500

    added methods

--- core/BaseCache.core.php
+++ core/BaseCache.core.php
@@ -5,6 +5,17 @@ class BaseCache extends Memcache {
     public function __construct(){
         $this->connect($_SERVER['MEMCACHE_HOST'],$_SERVER['MEMCACHE_PORT']);
     }//construct
+
+    public function get($k){
+        $k=md5($k);
+        return parent::get($k);
+    }//get
+
+    public function set($k,$v){
+        $k=md5($k);
+        return parent::set($k,$v,FALSE,0);
+    }//set
+
     
 }//BaseCache
 
