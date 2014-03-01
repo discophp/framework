2bf4c29dab5934048f4f556f67beb4d56714f788
Bradley Hamilton <bradleygoskimmin@gmail.com>
2014-03-01 16:15:59 -0500

    completed fire()

--- core/BaseEvent.core.php
+++ core/BaseEvent.core.php
@@ -3,26 +3,45 @@
 class BaseEvent {
 
     private $events=Array();
-    private $actions=Array();
 
-    public final function listen($event,$action){
-        $events[]=$event;
-        $actions[]=$action;
+    public final function listen($event,$action,$priority=0){
+        if(!isset($this->events[$event])){
+            $this->events[$event]=Array('actions'=>Array());
+        }//if
+
+        $this->events[$event]['actions'][$priority]=$action;
+
     }//listen
 
-    public final function fire($event,$data){
-        $events = array_intersect_key(Array($event),$this->events);
-        if(count($events)>0){
+    public final function fire($event,$data=null){
 
-            var_dump($events);
+        if(count(array_intersect_key(Array($event=>Array()),$this->events))>0){
 
-            //if($instance instanceof Closure){
-            //    $instance=call_user_func($instance);
-            //}//if
+            $keys = array_keys($this->events[$event]['actions']);
 
-            //return Disco::handle($instance,$method,$args);
+            if(count($keys)>1)
+                $keys = ksort($keys);
+
+            foreach($keys as $k){
+                $action = $this->events[$event]['actions'][$k];
+                if($action instanceof Closure){
+                    call_user_func($action);
+                }//if
+                else {
+                    $method='work';
+                    if(stripos($action,'@')!==false){
+                        $method = explode('@',$action);
+                        $action = $method[0];
+                        $method = $method[1];
+                    }//if
+                    $instance = new $action();
+                    Disco::handle($instance,$method,$data);
+                }//el
+            }//foreach
 
         }//if
+        else 
+            TRIGGER_ERROR('Event:: Event not found '.$event,E_USER_WARNING);
     }//fire
 
 }//BaseEvent
