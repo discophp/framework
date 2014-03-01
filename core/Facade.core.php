67f9339eb6ef7e9ce2c331f249721c3e4fa23351
Bradley Hamilton <bradleygoskimmin@gmail.com>
2014-02-28 19:58:05 -0500

    removed commented out code

--- core/Facade.core.php
+++ core/Facade.core.php
@@ -9,20 +9,6 @@ abstract class Facade {
      *      @param mixed $args
      *      @return mixed
      */
-    //public static function __callStatic($method,$args){
-    //    global $disco;
-
-    //    $instance = $disco->facades[static::returnFacadeId()];
-
-    //    if($instance instanceof Closure){
-    //        $instance=call_user_func($instance);
-    //        $disco->facades[static::returnFacadeId()]=$instance;
-    //    }//if
-
-    //    return Disco::handle($instance,$method,$args);
-
-    //}//callStatic
-
     public static function __callStatic($method,$args){
 
         $instance = Disco::$facades[static::returnFacadeId()];
