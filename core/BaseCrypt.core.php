9b4de7f46e117ffafa82add61b0a9432da32b036
Bradley Hamilton <bradleygoskimmin@gmail.com>
2014-02-28 19:23:26 -0500

    changed RIJNDAEL to use 256 key size

--- core/BaseCrypt.core.php
+++ core/BaseCrypt.core.php
@@ -2,38 +2,50 @@
 
 class BaseCrypt {
 
-    //encrypt AES128
+    /**
+     *      Encrypt with AES256
+     *      
+     *      @return string
+    */
     public function encrypt($input){
     
-        $cipher = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
-        
-        $key128 = $_SERVER["AES_KEY128"];
-        $iv = $_SERVER["AES_IV"];
+        $key256 = pack('H*',$_SERVER["AES_KEY256"]);
 
-        if (mcrypt_generic_init($cipher, $key128, $iv) != -1) {
-            $cipherText = mcrypt_generic($cipher,$input);
-            mcrypt_generic_deinit($cipher);
-        }//if
+        $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128,MCRYPT_MODE_CBC);
+        $iv = mcrypt_create_iv($iv_size,MCRYPT_RAND);
 
-        return bin2hex($cipherText);
+        $cipherText = mcrypt_encrypt(MCRYPT_RIJNDAEL_128,$key256,$input,MCRYPT_MODE_CBC,$iv);
+
+        return base64_encode($iv.$cipherText);
+
     }//AES128
 
-    //decrypt AES128
+
+    /**
+     *      Decrypt with AES256
+     *
+     *      @return string
+    */
     public function decrypt($crypt){
-        $cipher = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
 
-        $key128 = $_SERVER["AES_KEY128"];
-        $iv = $_SERVER["AES_IV"];
+        $key256 = pack('H*',$_SERVER["AES_KEY256"]);
 
-        if (mcrypt_generic_init($cipher, $key128, $iv) != -1) {
-            $cipherText = mcrypt_generic($cipher,$crypt);
-            mcrypt_generic_deinit($cipher);
-        }//end if
+        $cipherText = base64_decode($crypt);
 
-        return bin2hex($cipherText);
+        $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128,MCRYPT_MODE_CBC);
+        $iv = substr($cipherText,0,$iv_size);
+
+        $cipherText = substr($cipherText,$iv_size);
+
+        return mcrypt_decrypt(MCRYPT_RIJNDAEL_128,$key256,$cipherText,MCRYPT_MODE_CBC,$iv);
     }//end AES128
 
 
+    /**
+     *      hash with sha512
+     *      
+     *      @return string
+     */
     public function hash($pw){
         return hash('sha512',$_SERVER['SHA512_SALT_LEAD'].$pw.$_SERVER['SHA512_SALT_TAIL']);
     }//pwHash
