b2077a2ae9112943235760706e06d782bb59cd13
root <root@mint>
2014-03-05 18:58:03 -0500

    added method instance() to allow accessing the inner class the Facade is fronting for

--- core/Facade.core.php
+++ core/Facade.core.php
@@ -2,6 +2,15 @@
 
 abstract class Facade {
 
+
+    /**
+     *      classes that extend the Facade MUST IMPLEMENT 
+     *      this method!
+     */
+    abstract protected static function returnFacadeId();
+
+
+
     /**
      *      magic method __callStatic
      *
@@ -23,11 +32,26 @@ abstract class Facade {
     }//callStatic
 
 
+    /**
+     *      Return the instance of the object 
+     *
+     *      @return mixed
+     */
+    public static function instance(){
 
-    abstract protected static function returnFacadeId();
+        $instance = Disco::$facades[static::returnFacadeId()];
+
+        if($instance instanceof Closure){
+            $instance=call_user_func($instance);
+            Disco::$facades[static::returnFacadeId()]=$instance;
+        }//if
+
+        return $instance;
+
+    }//callStatic
 
 
 
-}//BaseInstanceController
+}//Facade
 
 ?>
