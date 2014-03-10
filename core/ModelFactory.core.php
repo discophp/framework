e15968e03fbf3bea86a9d16a367d5004a339c38a
root <root@mint>
2014-03-09 21:03:15 -0400

    finalized comments

--- core/ModelFactory.core.php
+++ core/ModelFactory.core.php
@@ -1,9 +1,30 @@
 <?php
+/**
+ *      This file holds the ModelFactory Class
+*/
 
+
+/**
+ *      ModelFactory class
+ *      Centeralized point of access to instances of models
+*/
 class ModelFactory {
 
+
+    /**
+     *      Model storage
+    */
     private $models=Array();
 
+
+
+    /**
+     *      Access a model
+     *
+     *
+     *      @param string $name the name of the model
+     *      @return object $this->models[$name] the instance of the Model
+    */
     public final function m($name){
         if(isset($this->models[$name]))
             return $this->models[$name];
