223713b968328665edb68a63a0e9b563f2d9d8c4
root <root@mint>
2014-03-09 20:45:19 -0400

    finalized comments

--- core/BaseSession.core.php
+++ core/BaseSession.core.php
@@ -1,10 +1,23 @@
 <?php
+/**
+ *      This file holds the BaseSession class
+*/
 
+
+/**
+ *      Convient wrapper around using $_SESSION variables.
+ *      Helps add a security layer around the use of sessions by checking for session hijacking
+*/
 class BaseSession {
+
+
 
     /**
      *      Start up our session
      *      and do some security checks
+     *
+     *
+     *      @return void
     */
     public function __construct(){
         session_start();
@@ -21,8 +34,10 @@ class BaseSession {
     }//construct
 
 
+
     /**
      *      does a session variable exist
+     *
      *
      *      @param mixed $k
      *      @return boolean
@@ -38,17 +53,22 @@ class BaseSession {
     /**
      *      set a session variable 
      *
+     *
      *      @param mixed $k
      *      @param mixed $v
+     *      @return void
     */
     public function set($k,$v){
         $_SESSION[$k]=$v;
     }//set
 
 
+
     /**
      *      get a session variable 
      *
+     *
+     *      @param string $k 
      *      @return mixed
     */
     public function get($k){
@@ -56,47 +76,67 @@ class BaseSession {
     }//get
 
 
+
     /**
      *      delete a session variable
      *
+     *
      *      @param mixed $k
+     *      @return void
     */
     public function remove($k){
         unset($_SESSION[$k]);
     }//remove
 
 
+
     /**
      *      regenerate a session id
      *      and keep the data
+     *
+     *
+     *      @return void
     */
     public function regen(){
         session_regenerate_id();
     }//regen
 
 
+
     /**
      *      reset a session
+     *
+     *
+     *      @return void
     */
     public function reset(){
         session_regenerate_id(true);
     }//reset
 
 
+
     /**
      *      Clear all session variables
+     *
+     *
+     *      @return void
     */
     public function flush(){
         session_unset();
     }//flush
 
 
+
     /**
      *      Destroy A session
+     *
+     *      
+     *      @return void
      */
     public function destroy(){
         session_destroy();
     }//destroy
+
 
 
 }//BaseSession
