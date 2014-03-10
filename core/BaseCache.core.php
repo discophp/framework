9bb80d0b7cd87878a1da043b41f6212a0caa4d95
root <root@mint>
2014-03-09 20:06:59 -0400

    added comments

--- core/BaseCache.core.php
+++ core/BaseCache.core.php
@@ -1,16 +1,51 @@
 <?php
+/**
+ *      This file contains the BaseCache class
+*/
 
+
+/**
+*       The BaseCache class extends Memcache
+*/
 class BaseCache extends Memcache {
 
+
+    /**
+     *      Make the connection to the memcached server
+     *
+     *
+     *      @return void
+    */
     public function __construct(){
         $this->connect($_SERVER['MEMCACHE_HOST'],$_SERVER['MEMCACHE_PORT']);
     }//construct
 
+
+
+    /**
+     *      Get a cached variable
+     *
+     *
+     *      @param string $k key used to access/store data
+     *
+     *      @return mixed
+    */
     public function get($k){
         $k=md5($k);
         return parent::get($k);
     }//get
 
+
+
+    /**
+     *      Set a variable in the cache
+     *
+     *
+     *      @param string $k the key to store the data with
+     *      @param mixed $v the value to store with the key
+     *
+     *      @return boolean
+    */
     public function set($k,$v){
         $k=md5($k);
         return parent::set($k,$v,FALSE,0);
