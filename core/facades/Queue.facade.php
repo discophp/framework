41e131f9c175f4b6ba7f3abdf05b903d6df567b1
Bradley Hamilton <bradleygoskimmin@gmail.com>
2014-05-29 19:26:28 -0400

    updating documentation

--- core/facades/Queue.facade.php
+++ core/facades/Queue.facade.php
@@ -1,11 +1,23 @@
 <?php
-
 class Queue extends Disco\classes\Facade {
 
     protected static function returnFacadeId(){
         return 'Queue';
     }//returnFacadeId
 
+    /**
+     * Get the jobs that are currently queued. Returns the jobs as objects
+     * structured like :
+     *  - pId = process ID of job.
+     *  - time = time job was enetered.
+     *  - delay = delay the job was told to wait before execution.
+     *  - object = the actual object or Closure that will execute the job.
+     *  - method = the method to call on the object.
+     *  - vars = the variables to be passed to the method.
+     *
+     *
+     *  @return array An array of job objects.
+    */
     public static function jobs(){
         exec('ps -ef | grep "php[[:space:]]\.\./disco[[:space:]]resolve"',$jobs);
 
@@ -34,6 +46,15 @@ class Queue extends Disco\classes\Facade {
     }//jobs
 
 
+
+    /**
+     * Kill a Queued job by passing its process ID number
+     *
+     *
+     * @param integer $pId The jobs process ID.
+     *
+     * @return boolean Whether or not the job was killed. 
+    */
     public static function killJob($pId){
 
         $j = self::jobs();
@@ -58,6 +79,4 @@ class Queue extends Disco\classes\Facade {
     }//killJob
 
 }//Queue
-
-
 ?>
--- core/manage/Manager.manage.php
+++ core/manage/Manager.manage.php
@@ -1,10 +1,35 @@
 <?php
-
 namespace Disco\manage;
+/**
+ * This file holds the Class Manager.
+*/
 
+
+/**
+ *  The Manager Class helps us manage our Disco Application. It primarly helps with getting and setting
+ *  data in your configuration files http://github.com/discophp/project/blob/master/.config.php .
+ *  It also helps you see what jobs you have waiting in your Queue and the ability to kill them. You can also do 
+ *  fun stuff with it like calling methods of your Applications objects via the command line to inspect their 
+ *  results.
+*/
 class Manager {
 
 
+    /**
+     * This function is very important to the functioning of the Queue class.
+     * It handles executing jobs that are pushed onto the Queue.
+     *
+     *
+     * @param integer $delay The amount of time to wait before executing the job.
+     * @param string|\Jeremeamia\SuperClosure\SerializableClosure $obj The \Closure to call the job on or the 
+     * name of the Facade to use.
+     * @param string $method The method name to call.
+     * @param string $vars The serialized form of the variables.
+     * @param string $d A serialized representation of our Application the moment the Job was pushed onto the 
+     * Queue.
+     *
+     * @return void
+    */
     public static function resolve($delay,$obj,$method,$vars,$d){
 
         $d = unserialize(base64_decode($d));
@@ -55,6 +80,13 @@ class Manager {
     }//resolve
 
 
+
+    /**
+     * List the jobs that are currently queued on the system.
+     *
+     *
+     * @return void
+    */
     public static function jobs(){
 
         $j = \Queue::jobs();
@@ -76,6 +108,15 @@ class Manager {
     }//jobs
 
 
+
+    /**
+     * Set or Get the APP_MODE in .config.php .
+     *
+     *
+     * @param string|null $mode The mode to put the app into;either DEV|PROD
+     *
+     * @return void
+    */
     public static function appMode($mode=null){
 
         if($mode!=null){
@@ -88,6 +129,15 @@ class Manager {
     }//appMode
 
 
+
+    /**
+     * Set a variable in .config.php .
+     *
+     * @param string $find  The variable to change.
+     * @param string $value The value to change the variable to.
+     *
+     * @return void
+    */
     public static function setConfig($find,$value){
         $find.='\'=>\'';
         $f = file_get_contents('./.config.php');
@@ -98,6 +148,15 @@ class Manager {
     }//setConfig
 
 
+
+    /**
+     * Get a variable in .config.php .
+     *
+     *
+     * @param string $find  The variable to find.
+     *
+     * @return string The value of $find.
+    */
     public static function getConfig($find){
         $find.='\'=>\'';
         $f = file_get_contents('./.config.php');
@@ -107,8 +166,14 @@ class Manager {
     }//setConfig
 
 
-
-
+    /**
+     * Generate a random number between passed $length.
+     *
+     *
+     * @param integer $length The length of the random string.
+     *
+     * @return string The random $length string.
+    */
     public static function genRand($length){
 
         $max = ceil($length/32);
@@ -123,28 +188,78 @@ class Manager {
     }//genRand
 
 
+    /**
+     *  Generate a 32 length AES256 Key
+     *
+     *
+     *  @return string 
+    */
     public static function genAES256Key(){
         return self::genRand(32);
     }//genAES256Key
 
+
+
+    /**
+     *  Set the AES256 Key in .config.php .
+     *
+     *
+     *  @param string $k The key.
+     *
+     *  @return void 
+    */
     public static function setAES256Key($k){
         self::setConfig('AES_KEY256',$k);
     }//setAES256KEY
 
+
+
+    /**
+     *  Generate a random string of length $l .
+     *
+     *
+     *  @param integer $l The length of the string to generate.
+     *
+     *  @return string 
+    */
     public static function genSalt($l){
         return self::genRand($l);
     }//genAES256Key
 
+
+    /**
+     *  Set the SHA512_SALT_LEAD Key in .config.php .
+     *
+     *
+     *  @param string $s The salt.
+     *
+     *  @return void 
+    */
     public static function setSaltLead($s){
         self::setConfig('SHA512_SALT_LEAD',$s);
     }//genAES256Key
 
+
+    /**
+     *  Set the SHA512_SALT_TAIL Key in .config.php .
+     *
+     *
+     *  @param string $s The salt.
+     *
+     *  @return void 
+    */
     public static function setSaltTail($s){
         self::setConfig('SHA512_SALT_TAIL',$s);
     }//genAES256Key
 
 
 
+    /**
+     * This function generates and sets the AES256 and SHA512 keys for .config.php after the composer install.
+     *
+     *
+     * @return void
+    */
     public static function install(){
 
         echo 'Setting AES_KEY256...';
@@ -162,9 +277,5 @@ class Manager {
 
     }//install
 
-
 }//Manager
-
-
-
 ?>
