58840198519707bc8c2f91a42382899cd3a73f75
Bradley Hamilton <bradleygoskimmin@gmail.com>
2014-06-16 12:43:37 -0400

    fixed instanceof call

--- core/classes/Event.class.php
+++ core/classes/Event.class.php
@@ -62,7 +62,7 @@ class Event {
 
             foreach($keys as $k){
                 $action = $this->events[$event]['actions'][$k];
-                if($action instanceof \Closure){
+                if($action instanceof \Jeremeamia\SuperClosure\SerializableClosure){
                     if($data==null){
                         return call_user_func($action);
                     }//if
