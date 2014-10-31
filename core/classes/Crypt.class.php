c45c806ef808bd770a6170879b48a2ac10b64265
Bradley Hamilton <bradleygoskimmin@gmail.com>
2014-10-31 17:08:22 -0400

    added timing safe comparision method

--- core/classes/Crypt.class.php
+++ core/classes/Crypt.class.php
@@ -75,5 +75,46 @@ class Crypt {
         return hash('sha512',$_SERVER['SHA512_SALT_LEAD'].$s.$_SERVER['SHA512_SALT_TAIL']);
     }//pwHash
 
+
+
+
+    /**
+     * Perform a timing safe compare.
+     *
+     *
+     * @param string $safe
+     * @param string $user
+     *
+     * @return boolean
+    */
+    public function timingSafeCompare($safe, $user) {
+    
+        // Prevent issues if string length is 0
+        $safe .= chr(0);
+        $user .= chr(0);
+        $safeLen = strlen($safe);
+        $userLen = strlen($user);
+    
+        // Set the result to the difference between the lengths
+        $result = $safeLen - $userLen;
+    
+        // Note that we ALWAYS iterate over the user-supplied length
+        // This is to prevent leaking length information
+        for ($i = 0; $i < $userLen; $i++) {
+            // Using % here is a trick to prevent notices
+            // It's safe, since if the lengths are different
+            // $result is already non-0
+            $result |= (ord($safe[$i % $safeLen]) ^ ord($user[$i]));
+        }//for
+    
+        // They are only identical
+        //strings if $result is
+        //exactly 0...
+        return $result === 0;
+    
+    }//timingSafeCompare
+
+
+
 }//BaseCrypt
 ?>
