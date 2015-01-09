a57e9260a611c503b46ad8a4b889c15d104e8985
Bradley Hamilton <brad@bradleyhamilton.com>
2015-01-08 21:52:42 -0500

    updating

--- test/travis_autoload.php
+++ test/travis_autoload.php
@@ -18,6 +18,6 @@ $app->config['SHA512_SALT_LEAD'] = 'weoi2309d';
 $app->config['SHA512_SALT_TAIL'] = 'skwero2309';
 
 $app->config['MOCK_DATA_STREAM'] = 'test/asset/mock-php-input-stream.txt';
-$app->config['PATH'] = dirname(__FILE__);
+$app->config['PATH'] = dirname(dirname(__FILE__));
 
 $app['Session']->has('test');
