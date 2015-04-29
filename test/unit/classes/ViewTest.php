112d0414e42adfef512cf7c89944fca6b8e35ae6
jbhamilton <brad@bradleyhamilton.com>
2015-04-29 11:01:05 -0400

    fixing view tests

--- test/unit/classes/ViewTest.php
+++ test/unit/classes/ViewTest.php
@@ -7,48 +7,24 @@ Class ViewTest extends PHPUnit_Framework_TestCase {
         View::instance()->html = Array();
 
         ob_start();
-        View::styleSrc('/css/css.css');
-        View::scriptSrc('/js/js.js');
+        View::lang('en');
         View::title('Test Title');
         View::desc('Test Desc');
         View::html('Content');
+        View::styleSrc('/css/css.css');
+        View::scriptSrc('/js/js.js');
         View::script('alert("Hi");');
-        View::lang('en');
         View::printPage();
         $c = ob_get_contents();
         ob_end_clean();
 
-        $actual = 
-"<!doctype html>
-    <html lang='en'>
-        <head>
-            <meta charset='utf-8' />
-            <meta content='index,follow' name='robots'>
-            <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-            <title>Test Title</title>
-            <meta name='description' content=\"Test Desc\">
-            <link type='image/x-icon' href='/favicon.png' rel='shortcut icon'>
-            <link rel='stylesheet' href='/css/css.css' type='text/css' />
-            
-            
-            
-        </head>
-        <body class=''>
-<div id='body-wrapper'>
-    <div id='header'></div>
-    <div id='body'>
-    Content
-    </div>
-    <div id='footer-spacing'></div>
-</div>
-<div id='footer'></div>
-
-        <script type='text/javascript' src='/js/js.js' ></script>
-        <script type='text/javascript'>alert(\"Hi\");</script>
-    </body>
-</html>";
-
-        $this->assertEquals($actual,$c);
+        $this->assertContains("<html lang='en'",$c);
+        $this->assertContains('<title>Test Title</title>',$c);
+        $this->assertContains("<meta name='description' content=\"Test Desc\">",$c);
+        $this->assertContains('Content',$c);
+        $this->assertContains("<link rel='stylesheet' href='/css/css.css' type='text/css'/>",$c);
+        $this->assertContains("<script type='text/javascript' src='/js/js.js'></script>",$c);
+        $this->assertContains("<script type='text/javascript'>alert(\"Hi\");</script>");
 
     }//testPageOutput
 
