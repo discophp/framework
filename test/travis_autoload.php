045204948ac9275bf7ba8a11716dd79ac6d17b79
jbhamilton <brad@bradleyhamilton.com>
2016-07-13 12:39:20 -0400

    Generate keys for travis CI

--- test/travis_autoload.php
+++ test/travis_autoload.php
@@ -5,3 +5,7 @@ require('autoload.php');
 \App::config('DB_USER','root');
 \App::config('DB_PASSWORD','');
 \App::config('DB_HOST','127.0.0.1');
+
+\App::config('AES_KEY256',\Disco\manage\Manager::genAES256Key());
+\App::config('SHA512_SALT_LEAD',\Disco\manage\Manager::genSalt(12));
+\App::config('SHA512_SALT_TAIL',\Disco\manage\Manager::genSalt(12));
